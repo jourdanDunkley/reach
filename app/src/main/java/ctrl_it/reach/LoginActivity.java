078447956e74b5590ad3a18056252a6ebc78506f
jordonocampbell <jordonocampbell@gmail.com>
2018-07-09 14:43:37 -0500

    Updated layout for login page, implemented the beginnings of a nav. drawer.
    Signed-off-by: jordonocampbell <jordonocampbell@gmail.com>

--- app/src/main/java/ctrl_it/reach/LoginActivity.java
+++ app/src/main/java/ctrl_it/reach/LoginActivity.java
@@ -1,17 +1,32 @@
 package ctrl_it.reach;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    private Button login_button, sign_up_button;
+    private EditText email_input, password_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent0 = new Intent(view.getContext(), DriverDashboardActivity.class);
+                startActivity(intent0);
+            }
+        });
+
+        sign_up_button = findViewById(R.id.sign_up_button);
+
     }
 }
